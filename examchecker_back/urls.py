738474ae1e5cf13b3a2f9f0d91ce8a7d25fdd3d3
Aslan <fenixx977@gmail.com>
2019-02-28 12:47:35 +0600

    fix permissions to api/docs

--- examchecker_back/urls.py
+++ examchecker_back/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.conf.urls import url
 from django.urls import include, path
+from rest_framework.permissions import AllowAny
 from rest_framework.documentation import include_docs_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('api.urls')),
-    url(r'api/docs', include_docs_urls(title='My API title'))
+    url(r'api/docs', include_docs_urls(title='My API title', permission_classes=[AllowAny]))
 ]
